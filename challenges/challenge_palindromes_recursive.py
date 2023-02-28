0779550f6809dce8be983219959f806139e51be2
Kevin Li <Kevinstart14@gmail.com>
2023-02-28 19:15:59 -0300

    feito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,4 +1,21 @@
 def is_palindrome_recursive(word, low_index, high_index):
+
+    if len(word) == 1:
+        return True
+    if not word:
+        return False
+    if low_index <= high_index:
+        if word[low_index] == word[high_index]:
+            low_index += 1
+            high_index -= 1
+            return is_palindrome_recursive(word, low_index, high_index)
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    print(is_palindrome_recursive("GG", 0, 1))
+
     # if word is None:
     #     return False
 
@@ -12,20 +29,3 @@ def is_palindrome_recursive(word, low_index, high_index):
     #     else:
     #         return False
     # return True
-
-    if low_index is None and high_index is None:
-        low_index = 0
-        high_index = len(word) - 1
-    if word is None:
-        return False
-    if low_index != high_index:
-        if word[low_index] == word[high_index]:
-            low_index += 1
-            high_index -= 1
-            is_palindrome_recursive(word, low_index, high_index)
-        return False
-    return True
-
-
-if __name__ == '__main__':
-    print(is_palindrome_recursive("ABCBA", 0, 4))
