ad24ed77bf22f503f1609fcd04e16fdfd40f92ad
Kevin Li <Kevinstart14@gmail.com>
2023-02-28 15:08:29 -0300

    feito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    contador = 0
+    try:
+        for permanent in permanence_period:
+            if permanent[0] <= target_time and permanent[1] >= target_time:
+                contador += 1
+        return contador
+    except TypeError:
+        return None
+
+
+if __name__ == '__main__':
+    print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1))
