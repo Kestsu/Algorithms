868661a7de2c62d22485accd75319ab5197e6ed2
Kevin Li <Kevinstart14@gmail.com>
2023-02-28 19:49:26 -0300

    removido erro de lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -15,17 +15,3 @@ def is_palindrome_recursive(word, low_index, high_index):
 
 if __name__ == '__main__':
     print(is_palindrome_recursive("GG", 0, 1))
-
-    # if word is None:
-    #     return False
-
-    # low_index = 0
-    # high_index = len(word) - 1
-
-    # while low_index != high_index:
-    #     if word[low_index] == word[high_index]:
-    #         low_index += 1
-    #         high_index -= 1
-    #     else:
-    #         return False
-    # return True
